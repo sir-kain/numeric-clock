89704cfa18434f8152268f02a226ba2cab406454
Waly <ahmadouwalyndiaye@gmail.com>
2021-07-25 20:01:52 +0000

    Handle color "c" property

--- src/components/stop-watch.ts
+++ src/components/stop-watch.ts
@@ -1,27 +1,33 @@
-import { css, LitElement, html } from "lit";
+import { css, LitElement, html, unsafeCSS } from "lit";
+import { styleMap } from 'lit/directives/style-map.js';
 import { customElement, property } from 'lit/decorators.js';
 import "./clock-number";
 
 @customElement('stop-watch')
 export class Stopwatch extends LitElement {
-  static styles = css`
-      :host {
-        align-items: center;
-        display: flex;
-      }
-      span {
-        font-size: 4rem;
-        margin: 0 .5rem;
-      }
-  `;
 
   @property({ type: Number }) _hours;
   @property({ type: Number }) _minutes;
   @property({ type: Number }) _seconds;
   @property({ type: Number }) _id;
+  @property({ type: String }) c;
+  styles = { color: '#000' };
 
+  static get styles() {
+    return css`
+    :host {
+      align-items: center;
+      display: flex;
+    }
+    span {
+      font-size: 4rem;
+      margin: 0 .5rem;
+    }
+  `;
+  }
   connectedCallback() {
     super.connectedCallback();
+    this.styles = { color: this.c };
     this._id = setInterval(() => {
       const currentDatetime = new Date();
       this._hours = currentDatetime.getHours().toLocaleString("en-US", {
@@ -47,11 +53,11 @@ export class Stopwatch extends LitElement {
 
   render() {
     return html`
-      <clock-number nb="${this._hours}"></clock-number>
-      <span>:</span>
-      <clock-number nb="${this._minutes}"></clock-number>
-      <span>:</span>
-      <clock-number nb="${this._seconds}"></clock-number>
+      <clock-number c="${this.c}" nb="${this._hours}"></clock-number>
+      <span style=${styleMap(this.styles)}>:</span>
+      <clock-number c="${this.c}" nb="${this._minutes}"></clock-number>
+      <span style=${styleMap(this.styles)}>:</span>
+      <clock-number c="${this.c}" nb="${this._seconds}"></clock-number>
     `;
   }
 }
