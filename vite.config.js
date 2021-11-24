41c847299ebff30cabe12e715afff75d6ea6f1bd
Waly <ahmadouwalyndiaye@gmail.com>
2021-11-24 12:38:36 +0000

    build production as library mode

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ const { defineConfig } = require("vite");
 
 module.exports = defineConfig({
   build: {
+    lib: {
+      entry: resolve(__dirname, "src/components/stop-watch.ts"),
+      name: 'MyLib',
+      // fileName: (format) => `my-lib.${format}.js`
+    },
     rollupOptions: {
       input: {
         demo: resolve(__dirname, "demo/index.html"),
