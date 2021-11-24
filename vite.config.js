559f04597343debc6c961ac87e27d8e9af69038f
Waly <ahmadouwalyndiaye@gmail.com>
2021-11-24 09:50:04 +0000

    Handle vite config for production

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        demo: resolve(__dirname, "demo/index.html"),
+      },
+    },
+  },
+});
